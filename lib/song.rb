122f0cd28633062dc4fbcc477bcdab536715a2f2
Delia Constantino <dconstan92@gmail.com>
2020-11-10 17:05:34 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -13,11 +13,11 @@ class Song
   end
 
   def artist_name
-    if self.artist.instance_variable_defined?(:@name)
-      self.artist.name
-    # else
-    #   nil
-    end
+    # if self.artist.instance_variable_defined?(:@name)
+    #   self.artist.name
+    # # else
+    # #   nil
+    # end
   end
 
   def self.all
