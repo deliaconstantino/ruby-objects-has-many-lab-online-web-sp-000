d80876088c6936e447e84ea092364ba821fea623
Delia Constantino <dconstan92@gmail.com>
2020-11-10 15:57:32 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,10 +1,15 @@
 class Artist
-  attr_accessor :name
+  attr_accessor :name, :songs
 
   def initialize(name)
     @name = name
+    @songs = []
   end
 
   def songs
   end
+
+  def add_song(song)
+    new_song = Song.new(song)
+    @songs << new_song
 end
